5f28c3ab42206194228ed7a375bacf73ac773300
BorislavBonevAlpha <gansdhika1@gmail.com>
2024-05-03 23:37:06 +0300

    Adding database script file

---
+++ solo_project/script_database.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, DateTime, Text, Float, Boolean, LargeBinary
+from sqlalchemy.orm import relationship
+from config.engine import Base
